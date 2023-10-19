28e25399f8f36c4819019053db3f09e777c831d6
Vasanthakumar912721106017 <146985344+Vasanthakumar912721106017@users.noreply.github.com>
2023-10-19 18:26:53 +0530

    Add files via upload

---
+++ CAD_phase_3 (1).py
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Travel Blog</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Your Travel Blog</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Destinations</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <section class="main-content">
+        <article>
+            <h2>Latest Travel Adventure</h2>
+            <img src="your-image.jpg" alt="Travel Image">
+            <p>Your captivating travel story goes here...</p>
+            <a href="blog-post.html">Read more</a>
+        </article>
+    </section>
+    
+    <aside>
+        <h2>Popular Destinations</h2>
+        <ul>
+            <li><a href="#">Destination 1</a></li>
+            <li><a href="#">Destination 2</a></li>
+            <li><a href="#">Destination 3</a></li>
+        </ul>
+    </aside>
+    
+    <footer>
+        &copy; 2023 Your Travel Blog
+    </footer>
+</body>
+</html>
+/* Reset some default browser styles */
+body, h1, h2, p {
+    margin: 0;
+    padding: 0;
+}
+
+
+/* Basic styling for the entire page */
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    margin: 0;
+    padding: 0;
+}
+
+
+/* Header styles */
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+}
+
+
+header h1 {
+    font-size: 32px;
+}
+
+
+nav ul {
+    list-style: none;
+}
+
+
+nav li {
+    display: inline;
+    margin-right: 20px;
+}
+
+
+nav a {
+    text-decoration: none;
+    color: #fff;
+}
+
+
+/* Main content area */
+.main-content {
+    width: 70%;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+
+/* Styling for blog posts */
+article {
+    margin-bottom: 20px;
+}
+
+
+article h2 {
+    font-size: 24px;
+}
+
+
+article img {
+    max-width: 100%;
+}
+
+
+article p {
+    font-size: 16px;
+}
+
+
+/* Sidebar styles */
+aside {
+    width: 30%;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+
+/* Footer styles */
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Boberick the llama</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Just testing this works!</h1>
+    </body>
+</html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Boberick the llama</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <main>
+            <section id="hero">
+                
+            </section>
+            <section id="about">
+                
+            </section>
+            <section id="contact">
+                
+            </section>
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Blog</title>
+</head>
+<body>
+    <!-- Content goes here -->
+</body>
+</html>
+<link rel="stylesheet" href="style.css">
+body {
+    font-family: Arial, sans-serif;
+}
+
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Travel Blog</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>My Travel Blog</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Destinations</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <section class="main-content">
+        <article>
+            <h2>Latest Adventure: A Journey to the Mountains</h2>
+            <img src="mountains.jpg" alt="Mountain Landscape">
+            <p>Join me on an epic adventure to the breathtaking mountains. Explore the stunning scenery and unique culture.</p>
+            <a href="blog-post.html">Read More</a>
+        </article>
+    </section>
+
+
+    <aside>
+        <h2>Popular Destinations</h2>
+        <ul>
+            <li><a href="#">Beach Paradise</a></li>
+            <li><a href="#">City Explorations</a></li>
+            <li><a href="#">Historical Sites</a></li>
+        </ul>
+    </aside>
+
+
+    <footer>
+        &copy; 2023 My Travel Blog
+    </footer>
+</body>
+</html>
+/* Reset some default browser styles */
+body, h1, h2, p {
+    margin: 0;
+    padding: 0;
+}
+
+
+/* Basic styling for the entire page */
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    margin: 0;
+    padding: 0;
+}
+
+
+Create Travel Blog Website
+
+
+Default (GPT-3.5)
+
+
+User
+
+
+
+
+Start by designing and developing the static travel blog website. 
+
+
+Design the website layout using HTML and CSS. Create engaging content with captivating photos and travel stories.
+
+
+ I'll provide an outline of how to design and develop the website:
+
+
+Plan Your Website:
+
+
+Decide on the website's purpose, target audience, and your niche within travel blogging.
+Choose a domain name and hosting service for your website.
+Design the Layout:
+
+
+Use HTML and CSS to create the structure and style of your website.
+Create a navigation menu, header, content area, and footer.
+Make it responsive to ensure it looks good on different screen sizes.
+Create HTML Structure:
+
+
+Set up the basic HTML structure with tags like <html>, <head>, and <body>.
+Define the title of your website in the <head> section.
+Design with CSS:
+
+
+Style your website using CSS. You can create an external CSS file and link it to your HTML document.
+Use CSS to format text, images, background, and layout.
+Navigation Menu:
+
+
+Create a navigation menu with links to different sections or pages of your blog.
+Content Creation:
+
+
+Write engaging travel stories and include captivating photos.
+Organize your content into blog posts or articles. Each post can be a separate HTML page.
+Image Optimization:
+
+
+Optimize your images for web use to ensure faster loading times.
+Use the <img> tag to insert images into your posts.
+SEO Optimization:
+
+
+Add relevant meta tags, keywords, and descriptions to improve SEO.
+Use proper heading tags (e.g., <h1>, <h2>) for titles and subtitles.
+Testing:
+
+
+Test your website on different browsers and devices to ensure compatibility.
+Check for broken links and make sure everything works as expected.
+Publish Your Website:
+
+
+Upload your HTML and CSS files to your hosting server using FTP or a hosting provider's control panel.
+Domain Setup:
+
+
+Configure your domain to point to your hosting server's IP address.
+Regular Updates:
+
+
+Keep your travel blog up to date with fres
